273a4d68afb2f1da16f6723d0c52caee66a819fb
hossain-adnan <63507205+hossain-adnan@users.noreply.github.com>
2024-10-24 11:56:01 +0600

    Write Basic Function

---
+++ script.js
@@ -0,0 +1,16 @@
+// Basic Function
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function devide(a, b) {
+    return a / b;
+}
