fc4bd9e6dab68445cb327c313e523f396e09ac34
hossain-adnan <63507205+hossain-adnan@users.noreply.github.com>
2024-11-01 09:52:53 +0600

    Make working function for Operator +

--- script.js
+++ script.js
@@ -4,11 +4,6 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a /b;
 
-//Variables
-let firstOperand;
-let operator;
-let secondOperand;
-
 //Operate Function
 function operate(firstOperand, secondOperand, operator) {
     return (operator === '+') ? add(firstOperand, secondOperand):
@@ -18,58 +13,73 @@ function operate(firstOperand, secondOperand, operator) {
      null;
 }
 
+//Buttons
+const one = document.querySelector('#one'); 
+const two = document.querySelector('#two'); 
+
+//Variables
+let firstOperand;
+let operator;
+let secondOperand;
+
 //Write on Screen
+let pressedEquals = false;
+let pressedOperator = false;
+
 const screen = document.querySelector('.screen');
-let arrForScreen = [];
-let displayValue; //type -> number
 
+let operandArray = [];
+let operandValue; //type -> number
 
+if (!pressedOperator) {
+
+    one.addEventListener('click', () => {
+        operandArray.push(1);
+        operandValue = +(operandArray.join(''));
+        screen.textContent = operandValue;
+
+        firstOperand = operandValue;
+    })
+
+    two.addEventListener('click', () => {
+        operandArray.push(2);
+        operandValue = +(operandArray.join(''));
+        screen.textContent = operandValue;
+
+        secondOperand = operandValue;
+    })
+}
+
+//When any operator is clicked
+//for add +
+let operatorRef = document.querySelector('#add');
+
+operatorRef.addEventListener('click', () => {
+
+    operandValue = null;
+    pressedOperator = true;
+
+    //stores the value of the operator
+    operator = '+' // operatorRef.value;
+    screen.textContent = '+' ;
+
+    console.log(operandValue);
+    console.log(pressedOperator);
+    console.log(operator);
+})
+
+//type equal
 const equals = document.querySelector('#equals')
 
 equals.addEventListener('click', () => {
     //stores inside the var secondOperand
-    // secondOperand = displayValue;
-    let solution = operate(1, 2, '+');
+    // secondOperand = operandValue;
+    let solution = operate(firstOperand, secondOperand, '+');
     //Show Solution on the display
     console.log(operate);
-    screen.textContent = solution;
+
+    screen.textContent = '';
+    // screen.textContent = solution;
 })
-
-const one = document.querySelector('#one'); //for 1
-one.addEventListener('click', () => {
-    arrForScreen.push(1);
-    displayValue = +(arrForScreen.join(''));
-    screen.textContent = displayValue;
-})
-
-//Type other numbers for the secondOperand
-const two = document.querySelector('#two'); //for 2
-two.addEventListener('click', () => {
-    arrForScreen.push(2);
-    displayValue = +(arrForScreen.join(''));
-    screen.textContent = displayValue;
-})
-
-//When any operator is clicked
-let operatorRef = document.querySelector('.add'); //for add +
-operatorRef.addEventListener('click', () => {
-
-    //stops storing inside the var firstOperand
-    firstOperand = displayValue;
-    displayValue = null;
-
-    //stores the value of the operator
-    operator.textContent = '+' // operatorRef.value;
-    screen.textContent = '+' ;
-
-    // //Type other numbers for the secondOperand
-    // const two = document.querySelector('#two'); //for 2
-    // two.addEventListener('click', () => {
-    //     arrForScreen.push(2);
-    //     displayValue = +(arrForScreen.join(''));
-    //     screen.textContent = displayValue; })
-})
-
-    //type equal
 
    
