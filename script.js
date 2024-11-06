7707d9d9374b04b858e3cfa25b663a05f1e2f174
hossain-adnan <63507205+hossain-adnan@users.noreply.github.com>
2024-11-06 12:03:22 +0600

    Use operator as equal when it's pressed again

--- script.js
+++ script.js
@@ -26,13 +26,13 @@ numbers.forEach(number => number.addEventListener('click', (e) => {
 
     screen.textContent = operandValue;
 
-    if (pressedOperator === false) {
-        firstOperand = operandValue;
+    if (pressedOperator === false) { 
+        firstOperand = operandValue; // No operator is pressed? each number's recorded as firstOperand
     } else {
         secondOperand = operandValue;
         // pressedOperator = false; 
     }
-}))
+}));
 
 //Point
 const point = document.querySelector('#point');
@@ -77,6 +77,14 @@ operatorRef.forEach(element => element.addEventListener('click', (e) => {
         firstOperand = solution;
     }
 
+    if (firstOperand != null && secondOperand != null){ // when operator is clicked instead of euals
+        equalsFunction();
+        pressedOperator = true; // to record firstOperand
+        if(firstOperand === null){
+            firstOperand = solution;
+        }
+    }
+
     operator = e.target.value;
     console.log(operator);
     screen.textContent = screen.textContent + operator;
@@ -84,8 +92,9 @@ operatorRef.forEach(element => element.addEventListener('click', (e) => {
 
 //Equal is pressed
 const equals = document.querySelector('#equals');
+equals.addEventListener('click', equalsFunction);
 
-equals.addEventListener('click', () => {
+function equalsFunction() {
     console.log(`firstOperand: ${firstOperand}`);
     console.log(`secondOperand: ${secondOperand}`);
     console.log(`operator: ${operator}`);
@@ -93,24 +102,25 @@ equals.addEventListener('click', () => {
     pressedEquals = true;
     operandArray = []; // Debug not showing more than 1 digit line 21
 
-    let solutionStr = (operate(firstOperand, secondOperand, operator)).toFixed(2);
-    let solutionNum = parseFloat(solutionStr);
+    //Number formatting
+        let solutionStr = (operate(firstOperand, secondOperand, operator)).toFixed(2);
+        let solutionNum = parseFloat(solutionStr);
 
-    if (solutionStr.replace('.', '').length > 6) { 
-        solution = solutionNum.toExponential(3); //scientific notation for large number
-    } else if (!Number.isInteger(solutionNum)) {
-        solution = solutionNum; // up to two decimal places
-    } else {
-        solution = Math.trunc(solutionNum); //Integer case with no decimals
-    }
+        if (solutionStr.replace('.', '').length > 6) { 
+            solution = solutionNum.toExponential(3); //scientific notation for large number
+        } else if (!Number.isInteger(solutionNum)) {
+            solution = solutionNum; // up to two decimal places
+        } else {
+            solution = Math.trunc(solutionNum); //Integer case with no decimals
+        }
     //Show Solution on the display
     screen.textContent = solution;
-    console.log(`screen: ${screen.textContent}`);
+    console.log(`solution: ${solution}`); //log to test
 
-    pressedOperator = false; //Undo any operator pressed 
+    pressedOperator = false; //clicking "=" undo any operator pressed 
     firstOperand = null;
     secondOperand = null;
-});
+};
 
 // AC button
 const autoClear = document.querySelector('#ac');
